c5f7fd7c614564e05d36b385e682b1a09214c1f2
BQ <chinbillybilbo@gmail.com>
2016-07-14 17:53:17 +0100

    two test window - vector & rect classes.

---
+++ src/twod/rect.hpp
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <twod/vector.hpp>
+
+namespace twod {
+    
+    template <typename T>
+    struct Rect
+    {
+        typedef Vec2<T> Vec;
+        
+        Vec tl, br;
+        
+        Rect() {}
+        Rect(T l, T t, T r, T b) : tl(l,t), br(r,b) {}
+        Rect(const Vec& tl_, const Vec& br_) : tl(tl_), br(br_) {}
+        
+        template <typename U>
+        Rect(const U& other) : tl(Vec2<T>(other.tl)), br(Vec2<T>(other.br)) {}
+
+        void set(const Vec& tl_, const Vec& br_) { tl = tl_, br = br_; }
+        
+        void set(T left, T top, T right, T bottom) {
+            set(Vec(left,top), Vec(right,bottom));
+        }
+        
+        T width() const     { return br.x - tl.x; }
+        T height() const    { return br.y - tl.y; }
+        Vec size() const    { return Vec(width(), height()); }
+        T area() const      { const T sz(size()); return sz.x * sz.y; }
+        
+        T left() const      { return tl.x; }
+        T right() const     { return br.x; }
+        T top() const       { return tl.y; }
+        T bottom() const    { return br.y; }
+        
+        Vec centre() const  { return Vec((tl.x+br.x)/2, (tl.y+br.y)/2); }
+        
+        Vec topLeft() const       { return tl; }
+        Vec topRight() const      { return Vec(br.x, tl.y); }
+        Vec bottomLeft() const    { return Vec(tl.x, br.y); }
+        Vec bottomRight() const   { return br; }
+        
+        Rect& operator += (const Rect &other) {
+            tl += other.tl;
+            br += other.br;
+            return *this;
+        }
+        
+        Rect& operator += (const Vec& vec) {
+            tl += vec;
+            br += vec;
+            return *this;
+        }
+        
+        Rect operator + (const Rect& other) {
+            return Rect(tl + other.tl, br + other.br);
+        }
+
+        Rect operator + (const Vec& vec) {
+            return Rect(tl + vec, br + vec);
+        }
+        
+        Rect& moveTo(const Vec& vec)    // absolute
+        {
+            const Vec sz(size());
+            tl = vec;
+            br = tl + sz;
+            return *this;
+        }
+        
+        Rect& moveBy(const Vec& vec)    // relative
+        {
+            tl += vec;
+            br += vec;
+            return *this;
+        }
+
+        bool contains(const Vec& pt) const {
+            return  !(pt.x < tl.x || pt.x > br.x || pt.y < tl.y || pt.y > br.y);
+        }
+
+        bool contains(const Rect& r) const {
+            return contains(r.tl) && contains(r.br);
+        }
+
+        // Ensure this inside container. Fails if container smaller.
+        bool clampInside(const Rect& container)
+        {
+            const auto szThis(size()), szCont(container.size());
+            
+            if (tl.x < container.tl.x)
+                tl.x = container.tl.x, br.x = container.tl.x + szThis.x;
+            else if (br.x > container.br.x)
+                tl.x = container.br.x - szThis.x, br.x = container.br.x;
+
+            if (tl.y < container.tl.y)
+                tl.y = container.tl.y, br.y = container.tl.y + szThis.y;
+            else if (br.y > container.br.y)
+                tl.y = container.br.y - szThis.y, br.y = container.br.y;
+
+            return szThis.x <= szCont.x && szThis.y <= szCont.y;
+        }
+        
+        // Ensure origin top-left.
+        Rect& normalise()
+        {
+            if (tl.x > br.x) std::swap(tl.x, br.x);
+            if (tl.y > br.y) std::swap(tl.y, br.y);
+            return *this;
+        }
+    };
+
+    typedef Rect<int32_t> Recti32;
+    typedef Recti32 Recti;
+    
+    typedef Rect<float> Rectf32;
+    typedef Rectf32 Rectf;
+    
+}
---
+++ src/twod/vector.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <cmath>
+#include <stdint.h>
+
+namespace twod {
+    
+    template <typename T, typename Enable = void> struct VectorTraits;
+
+    template <typename T>
+    struct VectorTraits<T, typename std::enable_if<std::is_integral<T>::value>::type>
+    {
+        static inline bool compare(T x1, T y1, T x2, T y2) {
+            return x1==x2 && y1==y2;
+        }
+    };
+
+    template <typename T>
+    struct VectorTraits<T, typename std::enable_if<std::is_floating_point<T>::value>::type>
+    {
+        constexpr static T c_eps = 1e-5;
+        static inline bool fltcmp(T a, T b) { return std::abs(a-b) < T(c_eps); }
+
+        static inline bool compare(T x1, T y1, T x2, T y2) {
+            return fltcmp(x1, x2) && fltcmp(y1, y2);
+        }
+    };
+
+    template <typename T>
+    struct Vec2
+    {
+        T x,y;
+
+        Vec2() : x(0), y(0) {}
+
+        Vec2(T x_, T y_) : x(x_), y(y_) {}
+        
+        template <typename U>
+        Vec2(const Vec2<U>& o) : x(T(o.x)), y(T(o.y)) {}
+        
+        bool operator == (const Vec2& o) const {
+            return VectorTraits<T>::compare(x, y, o.x, o.y);
+        }
+        
+        void set(T x_, T y_) { x = x_, y = y_; }
+        
+        Vec2 operator + (const Vec2& o) const   { return Vec2(x + o.x, y + o.y); }
+        void operator += (const Vec2& o)        { x += o.x, y += o.y; }
+        
+        Vec2 operator - () const                { return Vec2(-x, -y); }
+        Vec2 operator - (const Vec2& o) const   { return Vec2(x - o.x, y - o.y); }
+        void operator -= (const Vec2& o)        { x -= o.x, y -= o.y; }
+        
+        Vec2 operator * (T s) const             { return Vec2(x*s, y*s); }
+        void operator *= (T s)                  { x *= s, y *= s; }
+        Vec2 operator * (const Vec2& o) const   { return Vec2(x * o.x, y * o.y); }
+        void operator *= (const Vec2& o)        { x *= o.x, y *= o.y; }
+        
+        Vec2 operator / (T s) const             { return Vec2(x/s, y/s); }
+        void operator /= (T s)                  { x /= s, y /= s; }
+        
+        T lengthSquared() const     { return x*x + y*y; }
+        T length() const            { return std::sqrt(x*x + y*y); }
+
+        T dot(const Vec2 &o) const {
+            return x * o.x + y * o.y;
+        }
+
+        float normalise()
+        {
+            const float vlen = length();
+            const float invlen = 1.f / vlen;
+            x *= invlen;
+            y *= invlen;
+            return vlen;    // Old length.
+        }
+
+        float setLength(float newLen)
+        {
+            const float vlen = length();
+            const float scale = newLen / vlen;
+            x *= scale;
+            y *= scale;
+            return vlen;    // Old length.
+        }
+
+        static Vec2 polar(float rad);               // anti-clkws from x-axis.
+        static Vec2 compass(float degrees);         // clkws from north.
+
+        // Rotate vector origin anti-clkws by rad.
+        void rotate(float aclkRad);
+
+        // Rotate around centre point anti-clkws by rad.
+        void rotateAround(const Vec2 &centre, float aclkRad);
+    };
+
+    typedef Vec2<int16_t> Vec2i16;
+    typedef Vec2<int32_t> Vec2i32;
+    typedef Vec2i32 Vec2i;
+    
+    typedef Vec2<float> Vec2f32;
+    typedef Vec2<double> Vec2f64;
+    typedef Vec2f32 Vec2f;
+    
+}
