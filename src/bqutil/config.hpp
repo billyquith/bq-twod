d5db004d8d4dafcb54a9626c8dc0e55d37def53f
BQ <chinbillybilbo@gmail.com>
2016-10-01 11:35:35 +0100

    Remove bq-util and create bq-handy.

--- src/bqutil/config.hpp
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <cstdlib>
-
-#define BQ_ABORT(MSG) do{ fprintf(stderr, MSG); std::abort(); } while(0)
-
-#define BQ_ASSERT(TEST) if (!(TEST)) BQ_ABORT(#TEST " failed. ")
-
